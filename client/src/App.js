d314079d734e1d029f93f455d0822a63d4f6ba17
Maharba07 <maharba1707@gmail.com>
2023-11-26 00:17:49 -0400

    PI-Dogs

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/home" element={<Home/>}/>
-        <Route path="/details" element={<Detail/>}/>
+        <Route path="/home/:id" element={<Detail/>}/>
         <Route path="/create" element={<Create/>}/>
       </Routes>
     </div>
--- client/src/detail/detail.components.jsx
+++ client/src/detail/detail.components.jsx
@@ -1,17 +1,56 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./detail.styles.css"
+import { useParams } from "react-router-dom";
+import "./detail.styles.css";
 
-function Detail() {
-    return (
-      <div className="detail-page">
-        <h1>Estas en el Detail</h1>
-        <div className="botones-details">
-        <Link to="/home">
-            <button className="boton-generico">Retornar</button>
+const Detail = () => {
+  const { id } = useParams();
+  const [dog, setDog] = useState({});
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    fetch(`https://api.thedogapi.com/v1/breeds/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.name) {
+          setDog(data);
+          fetch(`https://api.thedogapi.com/v1/images/${data.reference_image_id}`)
+            .then((response) => response.json())
+            .then((imageData) => {
+              setImageUrl(imageData.url);
+            });
+        } else {
+          window.alert("Dog not found");
+        }
+      });
+    return setDog({});
+  }, [id]);
+
+  return (
+    <div className="details">
+      <pre>
+        <h1>Dog Details:</h1>
+        <Link to="/home/">
+          <button className="return-button">Return</button>
         </Link>
-        </div>
-      </div>
-    );
-  }
-  
-  export default Detail;
+        <h2>Name: {dog.name}</h2>
+        {dog.weight && (
+          <>
+            <h2>Peso: </h2>
+            <h3>Imperial: {dog.weight.imperial} --Lb</h3>
+            <h3>Metrico: {dog.weight.metric} --Kg</h3>
+          </>
+        )}
+        <h3>Temperament: {dog.temperament}</h3>
+        
+          <div>
+            <h3>Image:</h3>
+            <img src={imageUrl} alt="Dog" />
+          </div>
+       
+      </pre>
+    </div>
+  );
+};
+
+export default Detail;
