e70f5d35cdc5e3fff7223f1d37e33111ca1cd514
Maharba07 <maharba1707@gmail.com>
2024-01-10 20:23:11 -0400

    PI-Dogs

--- client/src/components/cards/cards.components.jsx
+++ client/src/components/cards/cards.components.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import Card from "../card/card.components";
 import "./cards.styles.css";
 
@@ -24,9 +24,9 @@ function Cards({ allDogs, searchString }) {
     if (orderBy.field === "name") {
       fieldA = a.name.toLowerCase();
       fieldB = b.name.toLowerCase();
-    } else if (orderBy.field === "weight") {
-      fieldA = parseInt(a.weight.metric.split(" - ")[0]);
-      fieldB = parseInt(b.weight.metric.split(" - ")[0]);
+    } else if (orderBy.field === "temperament") {
+      fieldA = a.temperament.toLowerCase();
+      fieldB = b.temperament.toLowerCase();
     }
 
     const orderFactor = orderBy.order === "asc" ? 1 : -1;
@@ -39,9 +39,9 @@ function Cards({ allDogs, searchString }) {
         Sort By Name{" "}
         {orderBy.field === "name" && orderBy.order === "asc" ? "↓" : "↑"}
       </button>
-      <button className="ordenar" onClick={() => toggleOrder("weight")}>
-        Sort By Weight{" "}
-        {orderBy.field === "weight" && orderBy.order === "asc" ? "↓" : "↑"}
+      <button className="ordenar" onClick={() => toggleOrder("temperament")}> 
+        Sort By Temperament{" "}
+        {orderBy.field === "temperament" && orderBy.order === "asc" ? "↓" : "↑"} 
       </button>
 
       <div className="cards-list">
